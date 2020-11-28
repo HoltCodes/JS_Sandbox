878a1aad90cb5bb52686804c8449dce0b7146737
Rogan_Holt <rogan_holt@Rogans-iMac.local>
2020-11-28 16:42:41 -0500

    update

--- app.js
+++ app.js
@@ -182,3 +182,171 @@
 // console.log(val);
 
 
+// const firstname = 'Rogan';
+// const lastname = 'Holt';
+// const age = '28';
+// const str = 'Hello my name is Rogan';
+// const tags = 'web design, web developer, programming';
+
+// let val;
+
+// val = firstname + lastname;
+
+// // Concatenation
+// val = firstname + ' ' + lastname;
+
+// // Append
+// val = 'Rogan ';
+// val += 'Holt';
+
+// val = 'Hello, my name ' + firstname + ' and I am ' + age;
+
+// // Escaping \ before '
+// val = 'That\'s awesome, I can\'t wait';
+
+// // Length
+// val = firstname.length;
+
+// // concat
+// val = firstname.concat(' ', lastname);
+
+// //change case
+// val = firstname.toUpperCase();
+// val = lastname.toLowerCase();
+
+// val = firstname[2];
+
+// //indexof()
+// val = firstname.indexOf('g');
+// val = firstname.lastIndexOf('h');
+
+// // charAt()
+// val = firstname.charAt('2');
+// // Get last char
+// val = firstname.charAt(firstname.length-1);
+
+// // substring()
+// val = firstname.substring(0, 4);
+
+// // slice()
+// val = firstname.slice(0,4);
+
+// //split()
+// val = str.split(' ');
+// val = tags. split(',');
+
+// // replace()
+// val = str.replace('Rogan' , 'Jack');
+
+// // includes()
+// val = str.includes('Hello');
+
+
+// console.log(val);
+
+// const name = 'Rogan'
+// const age = '28'
+// const job = 'web dev'
+// const city = 'Tampa'
+// let html;
+
+// // without template strings (es5 way)
+// html = '<ul><li>Name: ' + name + '</li><li>Age: ' + age + '</li><li>Job: '+ job + '</li><li>City: '+ city +' </li></ul>';
+
+// html = '<ul>' +
+//         '<li>Name: ' + name + '</li>' +
+//         '<li>Age: ' + age + '</li>' +
+//         '<li>Job: ' + job + '</li>' +
+//         '<li>City: ' + city + '</li>'
+//         '</ul>';
+
+// function hello(){
+//     return 'Hello';
+// }
+// // with template strings (es6)
+// html = `
+//   <ul>
+//     <li>Name: ${name}</li>
+//     <li>Age: ${age}</li>
+//     <li>Job: ${job}</li>
+//     <li>City: ${city}</li>
+//     <li>${2 + 2}</li>
+//     <li> ${hello()}</li>
+//     <li> ${age > 30 ? '0ver 30' : 'Under 30'}</li> 
+ 
+//   </ul>
+// `;
+
+// document.body.innerHTML = html;
+
+// Create some arrays
+const numbers = [43, 23, 34, 4, 1 ,64];
+const numbers2 = new Array(3, 434, 23, 235, 55);
+const fruit = ['Apple', 'Banna', 'Orange', 'Pear'];
+const mixed = [22, 'Hello', true, undefined, null, {a:1, b:1}, new Date()];
+
+let val;
+
+// // Get array length
+// val = numbers.length;
+// // Check if is array
+// val = Array.isArray('hello')
+// // falase
+// val = Array.isArray(numbers)
+// // true
+
+// // get single value
+// val = numbers[3];
+// val = numbers [0];
+// // insert into array
+// numbers[2] = 100;
+
+// // find index of value
+// val = numbers.indexOf(64);
+
+// // mutating arrays
+// // add on to end
+// numbers.push(250);
+// // add on to front
+// numbers.unshift(250);
+
+// // take off from the end
+// numbers.pop();
+// // take off from the front
+// numbers.shift();
+
+// // splice value
+// numbers.splice(1,1);
+
+// // reverse
+// numbers.reverse();
+
+// // concatenate arrya
+// val = numbers.concat(numbers2);
+
+// // sorting arrays
+// val = fruit.sort();
+// val = numbers.sort();
+
+// // use the "compare function"
+// val = numbers.sort(function(x, y){
+//     return x - y;
+// });
+
+// // reverse sort 
+// val = numbers.sort(function(x, y){
+//     return y - x;
+// });
+
+
+// find
+function over50(num){
+    return num > 50;
+}
+
+val = numbers.find(over50);
+
+
+
+console.log(numbers);
+console.log(val);
