e60bef561b096cf252305b03e0032d983dc36c73
Rogan_Holt <rogan_holt@Rogans-iMac.local>
2020-12-03 14:18:32 -0500

    update

--- app.js
+++ app.js
@@ -423,9 +423,85 @@
 //     do something else
 // }
 
-const id = 100;
+  const id = 100;
 
-// EQUAL TO
-if (id == 101){
-    console.log('CORRECT')
+// // EQUAL TO
+// if (id == 100){
+//     console.log('CORRECT')
+// }else {
+//     console.log('INCORRECT');
+// }
+
+// // NOT EQUAL TO
+// if (id != 101){
+//     console.log('CORRECT')
+// }else {
+//     console.log('INCORRECT');
+// }
+
+// // EQUAL TO VALUE AND TYPE
+// if (id === 100){
+//     console.log('CORRECT')
+// }else {
+//     console.log('INCORRECT');
+// }
+// // EQUAL TO VALUE AND TYPE
+// if (id !== 100){
+//     console.log('CORRECT')
+// }else {
+//     console.log('INCORRECT');
+// }
+
+// Test if undefined
+// if (typeof id !== 'undefined'){
+//     console.log(`The ID is ${id}`);
+// }else {
+//     console.log('NO ID');
+// }
+
+// Greater or less than
+//  if (id <= 100){
+//      console.log('CORRECT');
+//  }else {
+//      console.log('INCORRECT');
+//  }
+
+// IF ELSE
+const color = 'yellow';
+
+// if(color === 'red'){
+//     console.log('Color is red');
+// } else if(color === 'blue'){
+//     console.log('Color is blue');
+//  } else {
+//      console.log('Color is not red or blue');
+//  }
+
+
+// Logical operators 
+const name = 'Rogan';
+const age = 25;
+
+if (age > 0 && age < 12){
+    console.log(`${name} is a child`);
+} else if (age >=13 && age <= 19){
+    console.log (`${name} is a teenager`);
+} else {
+    console.log (`${name} is an adult`);
 }
+
+// or ||
+if(age < 16 || age > 65) {
+    console.log(`${name} can't run in the race`);
+} else {
+    console.log(`${name} is registered for the race`);
+}
+
+// Ternary operator
+console.log(id === 100 ? 'CORRECT' : 'INCORRECT');
+
+// WITHOUT BRACES (Dont recommened!)
+if (id ===100)
+console.log('CORRECT');
+else
+console.log(INCORRECT);
