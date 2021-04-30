a47ac618dc9c9168bf7764ec94721e021ef818ca
Holt.codes <rogan_rnh3210@ymail.com>
2021-04-30 10:55:02 -0400

    commit

--- app.js
+++ app.js
@@ -65,17 +65,22 @@
 
 // document.querySelector()
 
-console.log(document.querySelector('#task-title'));
-console.log(document.querySelector('.card-title'));
-console.log(document.querySelector('h5'));
+// console.log(document.querySelector('#task-title'));
+// console.log(document.querySelector('.card-title'));
+// console.log(document.querySelector('h5'));
 
-document.querySelector('li').style.color = 'red';
-document.querySelector('ul li').style.color = 'blue';
+// document.querySelector('li').style.color = 'red';
+// document.querySelector('ul li').style.color = 'blue';
 
-document.querySelector('li:last-child').style.color = 'red';
-document.querySelector('li:nth-child(3').style.color = 'yellow'
-document.querySelector('li:nth-child(4)').textContent = 'Hello World';
-document.querySelector('li:nth-child(odd)').style.background = '#ccc';
-document.querySelector('li:nth-child(even)').style.background = 
+// document.querySelector('li:last-child').style.color = 'red';
+// document.querySelector('li:nth-child(3').style.color = 'yellow'
+// document.querySelector('li:nth-child(4)').textContent = 'Hello World';
+// document.querySelector('li:nth-child(odd)').style.background = '#ccc';
+// document.querySelector('li:nth-child(even)').style.background = 
 
 
+// document.getElementsByClassName
+
+const items = document.getElementsByClassName('collection-item');
+console.log('items');
+console.log(items[0]);
