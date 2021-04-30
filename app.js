a904dcdb729277da09a27bf265577c7b0e788d97
Holt.codes <rogan_rnh3210@ymail.com>
2021-04-30 15:45:37 -0400

    commit

--- app.js
+++ app.js
@@ -131,60 +131,150 @@
 
 // console.log(items);
 
-let val;
+// let val;
 
-const list = document.querySelector('ul.collection');
-const listItem = document.querySelector('li.collection-item:first-child');
+// const list = document.querySelector('ul.collection');
+// const listItem = document.querySelector('li.collection-item:first-child');
 
-val = listItem;
-val = list;
+// val = listItem;
+// val = list;
 
-// Get child nodes
-val = list.childNodes;
-val = list.childNodes[0];
-val = list.childNodes[0].nodeName;
-val = list.childNodes[3].nodeType;
+// // Get child nodes
+// val = list.childNodes;
+// val = list.childNodes[0];
+// val = list.childNodes[0].nodeName;
+// val = list.childNodes[3].nodeType;
 
-// 1 - Element
-// 2 - Attribute (deprecated)
-// 3 - Text node
-// 8 - Comment
-// 9 - Document itself
-// 10 - Doctype 
+// // 1 - Element
+// // 2 - Attribute (deprecated)
+// // 3 - Text node
+// // 8 - Comment
+// // 9 - Document itself
+// // 10 - Doctype 
 
-// Get children element nodes
-// val = list.children;
-val = list.children[0];
-list.children[1].textContent = 'Hello';
-// Children of children
-list.children[3].children[0].id = 'test-link';
-val = list.children[3].children[0];
-
-
-// First Child
-val = list.firstChild;
-val = list.firstElementChild;
+// // Get children element nodes
+// // val = list.children;
+// val = list.children[0];
+// list.children[1].textContent = 'Hello';
+// // Children of children
+// list.children[3].children[0].id = 'test-link';
+// val = list.children[3].children[0];
 
 
-// Last Child
-val = list.lastChild;
-val = list.lastElementChild;
+// // First Child
+// val = list.firstChild;
+// val = list.firstElementChild;
 
 
-// Count child elements
-val = list.childElementCount;
+// // Last Child
+// val = list.lastChild;
+// val = list.lastElementChild;
 
-// Get parent node
-val= listItem.parentNode;
-val = listItem.parentElement;
-val = listItem.parentElement.parentElement;
 
-// Get next sibling
-val = listItem.nextSibling;
-val = listItem.nextSibling.nextElementSibling;
+// // Count child elements
+// val = list.childElementCount;
 
-// Get prev sibling
-val = listItem.previousSibling;
-val = listItem.previousElementSibling;
+// // Get parent node
+// val= listItem.parentNode;
+// val = listItem.parentElement;
+// val = listItem.parentElement.parentElement;
 
-console.log(val);
+// // Get next sibling
+// val = listItem.nextSibling;
+// val = listItem.nextSibling.nextElementSibling;
+
+// // Get prev sibling
+// val = listItem.previousSibling;
+// val = listItem.previousElementSibling;
+
+// console.log(val);
+
+// Create element
+// const li = document.createElement('li');
+
+// // Add class
+// li.className = 'collection-item';
+
+// // Add id
+// li.id = 'new-item';
+
+// // Add attribute
+// li.setAttribute('title', 'New Item');
+
+// // Create text node and append
+// li.appendChild(document.createTextNode('Hello World'));
+
+// // Create new link element
+// const link = document.createElement('a');
+// // Add classes
+// link.className = 'delete-item secondary-content';
+// // Add icon HTML
+// link.innerHTML = ' <i class="fa fa-remove"></i>';
+
+// // Append link into li
+// li.appendChild(link);
+
+// // Append li as child to ul
+// document.querySelector('ul.collection').appendChild(li);
+
+// console.log(li);
+
+
+// REPLACE ELEMENT 
+
+// Create Elemnt 
+// const newHeading = document.createElement('h2');
+// // Add id
+// newHeading.id = 'tast-title';
+// // New text node
+// newHeading.appendChild(document.createTextNode('Task List'));
+
+// // Get the old heading
+// const oldHeading = document.getElementById('tast-title');
+// // Parent
+// const cardAction = document.querySelector('.card-action');
+
+// // Replace
+// cardAction.replaceChild(newHeading, oldHeading);
+
+// // Remove ELEMENT
+// const lis = document.querySelectorAll('li');
+// const list = document.querySelector('ul');
+
+// // Remove list item
+// lis[0].remove();
+
+// // Remove by child 
+// list.removeChild(lis[3]);
+
+// console.log(newHeading);
+
+
+
+// // Classes and attr
+// const firstLi = document.querySelector('li:first-child');
+// console.log(firstli.childrem[0]);
+
+// let val;
+// // Classes
+// val = link.className;
+// val = link.classList;
+// val = link.classList[0];
+// link.classList.add('test');
+// link.classList.remvoe('test');
+// val = link;
+
+// // Attributes
+// val = link.getAttribute('href');
+// val = link.setAttribute('href', 'http://google.com');
+// link.setAttribute('title', 'Google');
+// val = link.hasAttribute('title');
+// link.removeAttribute('title');
+// val = link;
+
+// val = link.getAttribute
+
+
+// console.log(val);
+
+
